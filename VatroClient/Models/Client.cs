56b504a701c597d176cffe9e1b17b8d749d50268
Milan Gajic <boromirsrb@yahoo.com>
2026-06-25 21:39:15 +0200

    fe: listing clients and client detail

---
+++ VatroClient/Models/Client.cs
@@ -0,0 +1,19 @@
+namespace VatroClient.Models
+{
+
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string? City { get; set; } = String.Empty;
+        public string? Address { get; set; } = String.Empty;
+        public string? Email { get; set; } = String.Empty;
+        public string? Phone { get; set; } = String.Empty;
+        public string? Phone2 { get; set; } = String.Empty;
+        public string? Note { get; set; } = String.Empty;
+        public bool Referent { get; set; } = false;
+        public bool Archived { get; set; } = false;
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+
+    }
+}
---
+++ VatroClient/Models/Control.cs
@@ -0,0 +1,15 @@
+namespace VatroClient.Models
+{
+    public class Control
+    {
+        public int Id { get; set; }
+        public string Subject { get; set; } = String.Empty;
+        public string Duration { get; set; } = String.Empty;
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+        public DateTime NextCheck { get; set; }
+        public string? Note { get; set; } = String.Empty;
+        public bool Archive { get; set; } = false;
+
+        public Client Client = null!;
+    }
+}
